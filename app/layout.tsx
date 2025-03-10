03c254dcfb5d2ab74afcd6d0d1a48c3fe640ef6f
viml kumar <vimalkg@gmail.com>
2025-03-10 13:59:05 +0530

    mck82.vercel.app zikis8.vercel.app fb.com/ztrabc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import { ViewTransitions } from 'next-view-transitions';
 import { Analytics } from '@vercel/analytics/react';
 
